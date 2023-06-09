ecec8ef0bc66faf7ef34da3b2b15b20ca51096c7
StreamScribe <118761018+FlyNightSky@users.noreply.github.com>
2023-06-08 23:13:58 -0400

    Update script.jsx

--- src/app/script.jsx
+++ src/app/script.jsx
@@ -45,17 +45,23 @@ const Script = () => {
         <h1 className="absolute pb-36 font-bold text-5xl z-0">DetoxGPT</h1>
         <input
           id="api-key"
-          className="z-10 outline outline-1 outline-teal-200 rounded bg-transparent px-2 hover:px-5"
+          className="z-10 transition-all outline outline-1 outline-teal-200 rounded bg-transparent px-2 hover:px-5"
           placeholder="API KEY"
         />
         <input
           id="input"
-          className=" z-10 absolute mt-20 outline outline-1 outline-teal-200 rounded bg-transparent px-2 hover:px-5"
+          className=" z-10 transition-all absolute mt-20 outline outline-1 outline-teal-200 rounded bg-transparent px-2 hover:px-5"
           placeholder="Ask DetoxGPT"
         />
         <h2 className='absolute font-bold pt-40'>Can't click on the GPT box? click tab</h2>
       <footer className='absolute bottom-0 pb-2 text-blue-200 '>
-       <a class="text-2xl" href="https://detoxnetwork.repl.co/"><img src="https://new-navbar.detoxnetwork.repl.co/logo.svg" width="50"/></a>
+       <a class="text-2xl absolute ml-20" href="https://cognet.vercel.app">
+        <img src="https://cognet.vercel.app/logo.png" className='z-0 ' width="45"/> 
+        </a> 
+        <h1 className='bottom-0 ml-14 absolute pb-5'>+</h1> 
+        <a href='https://detoxnetwork.repl.co'>
+        <img src="https://new-navbar.detoxnetwork.repl.co/logo.svg" className='mr-24 z-10' width="50"/>
+        </a>
       </footer>
       </div>
     </div>
